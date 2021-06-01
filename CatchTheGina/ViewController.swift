83e9e226266af23b4c32aff4da8a68aa730c71c5
BakytDzhumabev <djumabaevb@gmail.com>
2021-06-01 21:20:28 +0600

    add 9 dino images

--- CatchTheGina/ViewController.swift
+++ CatchTheGina/ViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highscoreLabel: UILabel!
+    @IBOutlet weak var dino9: UIImageView!
+    @IBOutlet weak var dino8: UIImageView!
+    @IBOutlet weak var dino7: UIImageView!
+    @IBOutlet weak var dino6: UIImageView!
+    @IBOutlet weak var dino4: UIImageView!
+    @IBOutlet weak var dino5: UIImageView!
+    @IBOutlet weak var dino1: UIImageView!
+    @IBOutlet weak var dino3: UIImageView!
+    @IBOutlet weak var dino2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
